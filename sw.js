6ed3a230bc3dee68995b68868d54f75576ac1906
akasfei <akasfei@gmail.com>
2017-12-03 20:52:57 +0800

    Fixed sw-toolbox path

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ importScripts('sw-toolbox/sw-toolbox.js');
 const fileManifest = [{% for file in site.static_files %}{% if file.extname == ".jpg" or file.extname == ".png" or file.extname == ".svg" or file.extname == ".ico" %}
   "{{file.path | slice: 1, 999}}",{% endif %}{% endfor %}
   "index.html",
-  "sw/workbox-sw.prod.v2.1.2.js",
+  "sw-toolbox/sw-toolbox.js",
   "smooth-scroll/dist/js/smooth-scroll.polyfills.min.js"
 ];
 toolbox.router.default = toolbox.cacheFirst;
