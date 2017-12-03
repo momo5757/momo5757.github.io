5d6de70843a868bcfe2e1ef31a4a80f6f06c52de
akasfei <akasfei@gmail.com>
2017-12-03 19:08:47 +0800

    Now using sw-toolbox for precaching

--- sw.js
+++ sw.js
@@ -2,24 +2,12 @@
 layout: null
 title: wzhn.me
 ---
-importScripts('sw/workbox-sw.prod.v2.1.2.js');
+importScripts('sw-toolbox/sw-toolbox.js');
 const fileManifest = [{% for file in site.static_files %}{% if file.extname == ".jpg" or file.extname == ".png" or file.extname == ".svg" or file.extname == ".ico" %}
-  {
-    "url": "{{file.path | slice: 1, 999}}",
-    "revision": "{{file.path | slice: 1, 999 | md5}}"
-  },{% endif %}{% endfor %}
-  {
-    "url": "index.html",
-    "revision": "{{'index.html' | md5}}"
-  },
-  {
-    "url": "sw/workbox-sw.prod.v2.1.2.js",
-    "revision": "{{'sw/workbox-sw.prod.v2.1.2.js' | md5}}"
-  },
-  {
-    "url": "smooth-scroll/dist/js/smooth-scroll.polyfills.min.js",
-    "revision": "{{'smooth-scroll/dist/js/smooth-scroll.polyfills.min.js' | md5}}"
-  }
+  "{{file.path | slice: 1, 999}}",{% endif %}{% endfor %}
+  "index.html",
+  "sw/workbox-sw.prod.v2.1.2.js",
+  "smooth-scroll/dist/js/smooth-scroll.polyfills.min.js"
 ];
-const workboxSW = new self.WorkboxSW();
-workboxSW.precache(fileManifest);
+toolbox.router.default = toolbox.cacheFirst;
+toolbox.precache(fileManifest);
