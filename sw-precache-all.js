f9ea8b15d2741728a24115e6e2a56f0cf0507c9b
akasfei <akasfei@gmail.com>
2017-12-09 10:06:20 +0800

    Now using workbox as serivce worker

--- sw-precache-all.js
+++ sw-precache-all.js
@@ -3,7 +3,8 @@ layout: null
 title: wzhn.me
 ---
 importScripts('workbox-sw/workbox-sw.js');
-const fileManifest = [{% for f in site.precache %}
+const fileManifest = [{% for file in site.static_files %}{% if file.extname == ".jpg" or file.extname == ".png" or file.extname == ".svg" or file.extname == ".ico" %}
+  { url: "{{file.path}}", revision: "{{file.modified_time | date: '%s'}}" },{% endif %}{% endfor %}{% for f in site.precache %}
   {% assign i = site.static_files | where: "path", f %}{ url: "{{i[0].path}}", revision: "{{i[0].modified_time | date: '%s'}}" },{% endfor %}
   { url: "/index.html", revision: "{{'now' | date: '%s'}}" }
 ];
