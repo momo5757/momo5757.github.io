9a51e89db9a1711e9488f12a0c70ba88e88c19d5
akasfei <akasfei@gmail.com>
2018-02-22 12:39:47 +0800

    Update to workbox-sw 3.0 beta

--- sw-precache-all.js
+++ sw-precache-all.js
@@ -3,13 +3,27 @@ layout: null
 title: wzhn.me
 ---
 importScripts('workbox-sw/workbox-sw.js');
+importScripts('workbox-sw/workbox-cache-expiration.prod.js');
+importScripts('workbox-sw/workbox-core.prod.js');
+importScripts('workbox-sw/workbox-precaching.prod.js');
+importScripts('workbox-sw/workbox-routing.prod.js');
+importScripts('workbox-sw/workbox-strategies.prod.js');
 const fileManifest = [{% for file in site.static_files %}{% if file.extname == ".jpg" or file.extname == ".png" or file.extname == ".svg" or file.extname == ".ico" %}
   "{{file.path}}",{% endif %}{% endfor %}{% for f in site.precache %}
   {% assign i = site.static_files | where: "path", f %}"{{i[0].path}}",{% endfor %}
   { url: "/index.html", revision: "{{'now' | date: '%s'}}" }
 ];
 
-const workboxSW = new WorkboxSW({clientsClaim: true, skipWaiting: true});
-workboxSW.precache(fileManifest);
-workboxSW.router.registerRoute(/\/index\.html/, workboxSW.strategies.networkFirst());
-workboxSW.router.registerRoute(/\.(?:png|gif|jpg|jpeg|svg|ico|js)$/, workboxSW.strategies.cacheFirst());
+workbox.clientsClaim();
+workbox.skipWaiting();
+workbox.core.setCacheNameDetails({prefix: 'workbox', suffix: 'wzhn.me', precache: 'cache'});
+workbox.precaching.precacheAndRoute(fileManifest);
+workbox.routing.registerRoute(/\/index\.html/, workbox.strategies.staleWhileRevalidate());
+workbox.routing.registerRoute(/\.(?:png|gif|jpg|jpeg|svg|ico|js)$/, workbox.strategies.cacheFirst({
+  cacheName: 'workbox-cache-wzhn.me',
+  plugins: [
+    new workbox.expiration.Plugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+    })
+  ]
+}));
--- sw.js
+++ sw.js
@@ -3,16 +3,31 @@ layout: null
 title: wzhn.me
 ---
 importScripts('workbox-sw/workbox-sw.js');
-const fileManifest = [{% for f in site.precache %}
+importScripts('workbox-sw/workbox-cache-expiration.prod.js');
+importScripts('workbox-sw/workbox-core.prod.js');
+importScripts('workbox-sw/workbox-precaching.prod.js');
+importScripts('workbox-sw/workbox-routing.prod.js');
+importScripts('workbox-sw/workbox-strategies.prod.js');
+const minimalFiles = [{% for f in site.precache %}
   {% assign i = site.static_files | where: "path", f %}"{{i[0].path}}",{% endfor %}
   { url: "/index.html", revision: "{{'now' | date: '%s'}}" }
 ];
+const allFiles = [{% for file in site.static_files %}{% if file.extname == ".jpg" or file.extname == ".png" or file.extname == ".svg" or file.extname == ".ico" %}
+  "{{file.path}}",{% endif %}{% endfor %}
+];
 
-const workboxSW = new WorkboxSW({clientsClaim: true, skipWaiting: true});
-workboxSW.precache(fileManifest);
-workboxSW.router.registerRoute(/\/index\.html/, workboxSW.strategies.networkFirst());
-workboxSW.router.registerRoute(/\.(?:png|gif|jpg|jpeg|svg|ico|js)$/, workboxSW.strategies.cacheFirst());
-
-/*
-{% assign i = site.static_files | where: "path", "/workbox-sw/workbox-sw.js" %} {{ i[0] | inspect}}
- */
+workbox.clientsClaim();
+workbox.skipWaiting();
+//workbox.setConfig({debug: true});
+//workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
+workbox.core.setCacheNameDetails({prefix: 'workbox', suffix: 'wzhn.me', precache: 'cache'});
+workbox.precaching.precacheAndRoute(minimalFiles);
+workbox.routing.registerRoute(/\/index\.html/, workbox.strategies.staleWhileRevalidate());
+workbox.routing.registerRoute(/\.(?:png|gif|jpg|jpeg|svg|ico|js)$/, workbox.strategies.cacheFirst({
+  cacheName: 'workbox-cache-wzhn.me',
+  plugins: [
+    new workbox.expiration.Plugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+    })
+  ]
+}));
